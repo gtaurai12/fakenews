cc2dc060f3d61930050812141966c0c424c8c622
gtaurai12 <85948450+gtaurai12@users.noreply.github.com>
2021-07-02 19:31:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,63 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
+import nltk
 import pickle
-import time
+import re
+
+from flask import Flask, request, render_template
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+nltk.download('stopwords')
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
+
+port_stem = PorterStemmer()
+
+
+def stemming(content):
+    stemmed_content = re.sub('[^a-zA-Z]', ' ', content)
+    stemmed_content = stemmed_content.lower()
+    stemmed_content = stemmed_content.split()
+    stemmed_content = [port_stem.stem(word) for word in stemmed_content if not word in stopwords.words('english')]
+    stemmed_content = ' '.join(stemmed_content)
+    return stemmed_content
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
     '''
-    import random
-    n = random.randint(0, 1)
 
-    prediction = n
+    int_features = [str(x) for x in request.form.values()]
+    import pandas as pd
+    int_features = pd.DataFrame(int_features)
 
+    feature = int_features['content'] = int_features.loc[0, :] + ' ' + int_features.loc[1, :]
+    feature = feature.apply(stemming)
 
-    if (prediction == 0):
+    X = feature.values
+
+    vectorizer = TfidfVectorizer()
+    vectorizer.fit(X)
+    X = vectorizer.transform(X)
+
+    X__prediction = model.predict(X)
+
+    prediction = X__prediction
+
+    if X__prediction == 0:
         statement = 'The news is Real'
     else:
         statement = 'The news is Fake'
-        
-    time.sleep(5)
 
-    return render_template('index.html', prediction_text=statement)
+    return statement
+
 
 if __name__ == '__main__':
     app.run()
